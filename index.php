5dc2111688b9efae5db285678527a9bb7e9571d6
Bellararah <121237292+bellararah@users.noreply.github.com>
2023-11-08 21:00:33 -0300

    commit inicial do projeto

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Somos uma startup que forma talentos em tecnologia e empresas em diversidade e inclusão! Promovemos soluções que conectam ações afirmativas focadas em gente, cultura e gestão, de negócios que demamdam serviços de tecnologia, desenvolvendo times de multi diversidades como a inclusão de talentos LGBTQIAPN+, pessoas negras, pessoas PCD entre outros grupos!">
+    <title> ENGLOBE - A Águia do conhecimento! </title>
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+    <!-- FIM GOOGLE FONTS -->
+    <link rel="stylesheet" href="CSS/englobe.css">
+</head>
+
+<body>
+    <header>
+        <div class="limites-tela01">
+            <div class="logo-menu">
+                <a href="#">
+                    <img src="IMG/logoenglobe.png" alt="Logomarca Englobe">
+                </a>
+            </div>
+
+            <nav class="cabecalho-tela01">
+                <ul>
+                    <li><a href="#">A Englobe</a></li>
+                    <li><a href="#">Soluções</a></li>
+                    <li><a href="#">Projetos</a></li>
+                    <li><a href="#">O que vamos englobar hoje?</a></li>
+                </ul>
+            </nav>
+
+            <div class="botao-login-tela01">
+                <a href="#">
+                    <button>Login</button>
+                </a>
+            </div>
+        </div> <!--  tag limites-tela01 -->
+
+
+
+    </header>
+
+    <main class="conteudo-tela01">
+        <section class="conteudo-principal-tela01">
+            <img class="conteudo-principal-imagem-tela01" src="IMG/img-tela-01A.png"
+                alt="imagem pagina inicial englobe">
+
+            <nav>
+                <div class="botao-lets-bora-englobar-tela01"></div>
+                <button>Let's bora englobar!</button>
+            </nav>
+        </section>
+
+        <section class="conteudo-secundario-tela01"><br><br><br>
+            <h3 class="conteudo-secundario-titulo">O que a ENGLOBE faz para ajudar voce a crescer ? </h3><br><br><br>
+            <P class="conteudo-secundario-paragrafo">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas
+                praesentium dolore porro iste doloremque nostrum saepe, sunt laudantium excepturi facilis asperiores
+                nemo ipsum necessitatibus, delectus rem. Consectetur beatae explicabo voluptatibus?</P> <br>
+            <P class="conteudo-secundario-paragrafo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur,
+                quis distinctio amet odio, non ex eligendi dicta illo vitae, eius totam ducimus. Quisquam assumenda
+                debitis sequi atque inventore harum odit?</P> <br>
+            <P class="conteudo-secundario-paragrafo">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex,
+                officiis atque earum quaerat quam culpa itaque accusantium voluptates? Ullam eos necessitatibus tempora
+                illo veniam ad accusamus, laudantium nihil deleniti nostrum.</P><br><br><br><br>
+        </section>
+
+        <section class="Formulario">
+            <div class="Formulario"></div>
+            <h1 class="título"> Fale Conosco</h1>
+            <form action="">
+                <input type="text" name="" placeholder="Digite seu nome completo" required>
+                <input type="text" name="" placeholder="Digite seu e-mail" required>
+                <input type="text" name="" placeholder=" Digite seu celular" required>
+                <textarea name="" id="" cols="30" rows="10" placeholder="Digite um comentário"></textarea>
+                <div class="botao-lets-bora-englobar-tela01"><input type="submit" value="Let's bora englobar!"></div>
+            </form>
+
+        </section>
+    </main>
+    <footer class="rodape-tela01">
+        <img class="rodape-imagem-tela01" src="IMG/logo Englobe.png">
+    </footer>
+</body>
+
+</html>
